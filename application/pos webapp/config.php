46c71c1087b7afa32d758a5126f26cf89e5acfe3
Carlos Ligligen <69950891+CLigligen@users.noreply.github.com>
2023-08-20 13:38:27 +0800

    Bregghan POS System

---
+++ application/pos webapp/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// $servername = "13.213.119.146";
+// $username = "pos";
+// $password = "faceloan0099";
+// $dbname = "pos";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pos";
